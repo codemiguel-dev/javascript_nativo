ef92fff38a5d6809f1b4f9f683bd07eb63be2315
codemiguel-dev <jose.calderon15@inacapmail.cl>
2025-04-02 19:30:33 -0300

    update

--- Software_design_web/js/script.js
+++ Software_design_web/js/script.js
@@ -1,19 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Crear un bloque de contenido de ejemplo
-    const designArea = document.getElementById("design-area");
 
-    // Crear un botón para añadir un bloque
-    const addBlockButton = document.createElement("button");
-    addBlockButton.textContent = "Añadir bloque";
-    addBlockButton.onclick = function() {
-        const newBlock = document.createElement("div");
-        newBlock.classList.add("content-block");
-        newBlock.innerHTML = "<p>Nuevo bloque de contenido</p>";
-        newBlock.style.backgroundColor = "#e2e2e2";
-        newBlock.style.padding = "20px";
-        newBlock.style.marginBottom = "10px";
-        designArea.appendChild(newBlock);
-    };
 
-    document.body.appendChild(addBlockButton);
-});
+// Get all menu from document
+document.querySelectorAll('.buttonTrigger').forEach(attachMenuEvent);
+
+// Menu Open and Close function
+function attachMenuEvent(menuButton) {
+  menuButton.addEventListener('click', function (e) {
+    e.preventDefault(); // Ngăn chặn hành vi mặc định
+    this.parentElement.classList.toggle('active');
+    this.classList.toggle('active');
+  });
+}
