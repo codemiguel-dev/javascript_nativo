1b09262e36b865e8ed0e9db7fc563319ac290228
codemiguel-dev <jose.calderon15@inacapmail.cl>
2025-04-07 16:51:47 -0400

    update

--- Software_design_web/public/js/components/card.js
+++ Software_design_web/public/js/components/card.js
@@ -10,4 +10,17 @@ export function generateCardlHTML(title, header) {
 </div>
     `;
   }
+
+export function generateCardlinklHTML(title, header) {
+    return `
+    <div class="card" style="width: 18rem;">
+  <img src="file/carousel/teclado.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    `;
+  }
   
