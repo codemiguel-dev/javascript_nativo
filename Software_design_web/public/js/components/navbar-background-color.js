71861650d28e6289be838d79dc1bceacb16c2eb2
codemiguel-dev <jose.calderon15@inacapmail.cl>
2025-04-17 20:05:28 -0400

    update

--- Software_design_web/public/js/components/navbar-background-color.js
+++ Software_design_web/public/js/components/navbar-background-color.js
@@ -1,4 +1,4 @@
-export function generateNavbarHTML(color) {
+export function generateNavbarHTML(color, opc1, opc2, opc3) {
   // Determina la clase de texto según el color de fondo
   const textClass = color === "light" ? "text-dark" : "text-light";
 
@@ -12,7 +12,13 @@ export function generateNavbarHTML(color) {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link ${textClass} active" href="#">Inicio</a>
+              <a class="nav-link ${textClass} active" href="#">${opc1}</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link ${textClass} active" href="#">${opc2}</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link ${textClass} active" href="#">${opc3}</a>
             </li>
           </ul>
         </div>
--- Software_design_web/public/js/configuration_navbar.js
+++ Software_design_web/public/js/configuration_navbar.js
@@ -6,21 +6,26 @@ import { guardarJSON } from './function/save_json.js';
 import { showJSON } from './function/show_page.js';
 import { shownavbarJSON } from './function/show_navbar.js';
 
-document.getElementById("updateColorbackground").addEventListener("click", () => {
+document.getElementById("updateNav").addEventListener("click", () => {
 
   if (!pageData) {
     console.log("pageData no está disponible");
     return;
   }
   const selectedRadio = document.querySelector('input[name="radioGroup"]:checked');
+  const opc1 = document.getElementById("opc1").value;
+  const opc2 = document.getElementById("opc2").value;
+  const opc3 = document.getElementById("opc3").value;
+
 
   if (!selectedRadio) {
     console.log("Ningún radio seleccionado");
     return;
   }
   // 1. Generar el HTML del carrusel con los valores
-  const navHTML = generateNavbarHTML(selectedRadio.value);
-  console.log(navHTML)
+  const navHTML = generateNavbarHTML(selectedRadio.value, opc1, opc2, opc3);
+  // console.log(opc1)
+  // console.log(navHTML)
 
   // 2. Verificar si el HTML ya existe (en container.html, no en title.html)
   if (pageData.page.navbar.html.includes(navHTML)) {
@@ -44,7 +49,6 @@ document.getElementById("updateColorbackground").addEventListener("click", () =>
   }
 
 });
-
 
 
 
