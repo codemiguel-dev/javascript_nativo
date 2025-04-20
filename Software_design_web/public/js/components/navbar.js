144c9143810fa1690f23f03032c579cf209057ac
codemiguel-dev <jose.calderon15@inacapmail.cl>
2025-04-19 23:13:02 -0400

    update

--- Software_design_web/public/js/components/navbar.js
+++ Software_design_web/public/js/components/navbar.js
@@ -1,12 +1,14 @@
-export function generateNavbarHTML(color, opc1, opc2, opc3) {
+export function generateNavbarHTML(color, opc1, opc2, opc3, opc4, opc5, opc6) {
   // Determina la clase de texto según el color de fondo
   const textClass = color === "light" ? "text-dark" : "text-light";
+  const buttoncolorClass = color === "light" ? "navbar-light" : "navbar-dark";
 
+  console.log(buttoncolorClass);
   return `
     <nav class="navbar navbar-expand-lg bg-${color}">
       <div class="container-fluid">
         <a class="navbar-brand ${textClass}" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+        <button class="navbar-toggler ${buttoncolorClass}" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -19,6 +21,15 @@ export function generateNavbarHTML(color, opc1, opc2, opc3) {
             </li>
             <li class="nav-item">
               <a class="nav-link ${textClass} active" href="#">${opc3}</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link ${textClass} active" href="#">${opc4}</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link ${textClass} active" href="#">${opc5}</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link ${textClass} active" href="#">${opc6}</a>
             </li>
           </ul>
         </div>
--- Software_design_web/public/js/configuration_navbar.js
+++ Software_design_web/public/js/configuration_navbar.js
@@ -1,7 +1,7 @@
 // Importar las variables desde el archivo components/navbar-primary.js
 import { 
   generateNavbarHTML, 
-  } from './components/navbar-background-color.js';
+  } from './components/navbar.js';
 import { guardarJSON } from './function/save_json.js';
 import { showJSON } from './function/show_page.js';
 import { shownavbarJSON } from './function/show_navbar.js';
@@ -16,6 +16,16 @@ document.getElementById("updateNav").addEventListener("click", () => {
   const opc1 = document.getElementById("opc1").value;
   const opc2 = document.getElementById("opc2").value;
   const opc3 = document.getElementById("opc3").value;
+  const opc4 = document.getElementById("opc4").value;
+  const opc5 = document.getElementById("opc5").value;
+  const opc6 = document.getElementById("opc6").value;
+
+  const opc1link = document.getElementById("opc1link").value;
+  const opc2link = document.getElementById("opc2link").value;
+  const opc3link = document.getElementById("opc3link").value;
+  const opc4link = document.getElementById("opc4link").value;
+  const opc5link = document.getElementById("opc5link").value;
+  const opc6link = document.getElementById("opc6link").value;
 
 
   if (!selectedRadio) {
@@ -23,7 +33,7 @@ document.getElementById("updateNav").addEventListener("click", () => {
     return;
   }
   // 1. Generar el HTML del carrusel con los valores
-  const navHTML = generateNavbarHTML(selectedRadio.value, opc1, opc2, opc3);
+  const navHTML = generateNavbarHTML(selectedRadio.value, opc1, opc2, opc3, opc4, opc5, opc6);
   // console.log(opc1)
   // console.log(navHTML)
 
